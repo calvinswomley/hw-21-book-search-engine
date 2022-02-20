63137818886667fdb8b217d6b9fcdd2e88954e75
Calvin Swomley <calvinswomley@gmail.com>
2022-02-20 10:31:45 -0700

    type defs update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [author]!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+    saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
     removeBook(bookId: String!): User
   }
   type User {
@@ -15,12 +15,12 @@ const typeDefs = gql`
     username: String
     email: String
     bookCount: Int
-    savedBooks: [Book]
+    savedBooks: [String]
   }
   type Book {
     _id: ID
     bookId: String
-    authors: [author]
+    authors: [String]
     description: String
     title: String
     image: String
