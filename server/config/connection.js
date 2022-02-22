57f2fbd8c0661858f0c369d7c51cc3183996a957
Calvin Swomley <calvinswomley@gmail.com>
2022-02-22 11:38:42 -0700

    env string update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,
+             {
+               useNewUrlParser: true,
+               useUnifiedTopology: true,
+             }
+     )
+     .then(() => console.log("MongoDB has been connected"))
+     .catch((err) => console.log(err));
+
+/*
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
+*/
 module.exports = mongoose.connection;
