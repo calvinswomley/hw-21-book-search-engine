3c86e67c401162a6f3ce262674f135c3fd8c802f
Calvin Swomley <calvinswomley@gmail.com>
2022-02-23 13:31:41 -0700

    apollo server install

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING,
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/googlebooks',
              {
                useNewUrlParser: true,
                useUnifiedTopology: true,
+               useCreateIndex: true,
+              useFindAndModify: false,
              }
      )
      .then(() => console.log("MongoDB has been connected"))
